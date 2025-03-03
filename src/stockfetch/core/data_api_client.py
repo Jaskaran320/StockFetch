bffb0646db88fa056d0b94f2bbcf04b607f46232
Aditya <ahujaaditya00@gmail.com>
2025-03-03 15:08:02 +0530

    OHLC logic fix.

--- src/stockfetch/core/data_api_client.py
+++ src/stockfetch/core/data_api_client.py
@@ -1,16 +1,22 @@
 # Factory class for different Stock Market APIs
-from abc import ABC, abstractmethod
+from __future__ import annotations
+
+import datetime
+import json
+import os
+import pickle
+from abc import ABC
+from abc import abstractmethod
+
 import pandas
 
 
 class DataAPIClient(ABC):
     def __init__(self):
-        print("Stock API Client initialized ✅")
-        
-        
-        
+        print('✅ Stock API Client initialized')
+
     # API Provider management
-    def _set_API_key(self,API_key:str):
+    def _set_API_key(self, API_key: str):
         """_summary_
         Set API key (if any) for API.
         Args:
@@ -19,7 +25,7 @@ class DataAPIClient(ABC):
 
     # API Request handling
     @abstractmethod
-    def _build_request(self,url:str ,params:dict) -> str: 
+    def _build_request(self, url: str, params: dict) -> str:
         """_summary_
         Abstract method to build request for fetching stock data.
         Args:
@@ -29,12 +35,9 @@ class DataAPIClient(ABC):
         Returns:
             str: endpoint url with parameters
         """
-        pass
 
-        pass
-    
     @abstractmethod
-    def get_market_holidays(self) -> dict: 
+    def get_market_holidays(self) -> dict:
         """_summary_
         Abstract method to get market holidays.
         Returns:
@@ -43,7 +46,9 @@ class DataAPIClient(ABC):
         pass
 
     @abstractmethod
-    def get_historical_ohlc_data(self, symbol:str, start_date:str, end_date:str) -> pandas.DataFrame:
+    def get_historical_ohlc_data(
+        self, symbol: str, start_date: str, end_date: str, data_folder_path: str,
+    ) -> pandas.DataFrame:
         """_summary_
         Abstract method to get equity OHLC data.
         Args:
@@ -55,9 +60,71 @@ class DataAPIClient(ABC):
             pandas.DataFrame: OHLC data
         """
         pass
-    #Todo: Add generic caching methods.
 
-    
-        
-        
-    
+    # API Response Handling
+
+    def _dump_data(
+        self,
+        data: dict | pandas.DataFrame | list | str,
+        file_name: str,
+        data_folder_path: str,
+        compress: bool = False,
+    ) -> None:
+        """_summary_
+        Dump data to file.
+        Args:
+            data (dict | pandas.DataFrame | list): Data to dump
+            file_name (str): File name
+            data_folder_path (str): Path to data folder
+            compress (bool, optional): Compress data. Defaults to False.
+        """
+
+        if os.path.exists(data_folder_path):
+            print('📁 Data folder exists')
+        else:
+            print('📁 New Folder created')
+            os.mkdir(data_folder_path)
+
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d')
+        data_file_name = f"{file_name}_{timestamp}"
+        extension = None
+
+        if compress:
+            extension = '.pkl'
+            pickle.dump(
+                data,
+                open(
+                    os.path.join(
+                        data_folder_path,
+                        data_file_name + extension,
+                    ), 'wb',
+                ),
+            )
+        elif isinstance(data, pandas.DataFrame):
+            extension = '.csv'
+            data.to_csv(
+                os.path.join(
+                    data_folder_path,
+                    data_file_name + extension,
+                ),
+            )
+        elif isinstance(data, dict):
+            extension = '.json'
+            json.dump(
+                data,
+                open(
+                    os.path.join(
+                        data_folder_path,
+                        data_file_name + extension,
+                    ), 'w',
+                ),
+            )
+        else:
+            extension = '.txt'
+            with open(
+                os.path.join(
+                    data_folder_path,
+                    data_file_name + extension,
+                ), 'w+',
+            ) as f:
+                f.write(data)
--- tests/test-script.py
+++ tests/test-script.py
@@ -1,11 +1,14 @@
-import stockfetch.core.data_api_client as data_api_client
+from __future__ import annotations
+
 import stockfetch.api.NSE_Client as NSE_Client
 
 if __name__ == '__main__':
-    print("Testing script")
+    print('Testing script')
     nse_client_instance = NSE_Client.NSE_Client()
-    symbol = "TATACONSUM"
-    start_date = "01-02-2000"
-    end_date = "01-01-2001"
-    df = nse_client_instance.get_historical_ohlc_data(symbol,start_date,end_date)
-    print(df)
+    symbol = 'TATACONSUM'
+    start_date = '01-02-2024'
+    end_date = '01-05-2024'
+    df = nse_client_instance.get_historical_ohlc_data(
+        symbol, start_date, end_date, data_folder_path='C:\\Users\\ahuja\\Desktop\\Projects\\StockFetch\\StockFetch\\tests\\dumps',
+    )
+    print(df.head())
